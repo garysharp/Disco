garysharp/Disco

commit b17ed91fb00c6606df94b407fc0355a8b43deb76

Update: Additional UI Extension Hooks

Disco.Web/Areas/Config/Controllers/DeviceProfileController.cs
@@ -8,6 +8,8 @@ using Disco.BI;
 using Disco.BI.Extensions;
 using Disco.Services.Plugins.Features.CertificateProvider;
 using Disco.Services.Plugins;
+using Disco.Services.Plugins.Features.UIExtension;
+using Disco.Models.UI.Config.DeviceProfile;
 
 namespace Disco.Web.Areas.Config.Controllers
 {
@@ -45,39 +47,55 @@ namespace Disco.Web.Areas.Config.Controllers
                 //m.OrganisationalUnit = m.DeviceProfile.OrganisationalUnit;
                 //m.ComputerNameTemplate = m.DeviceProfile.ComputerNameTemplate;
 
+                // UI Extensions
+                UIExtensions.ExecuteExtensions<ConfigDeviceProfileShowModel>(this.ControllerContext, m);
+
                 return View(MVC.Config.DeviceProfile.Views.Show, m);
             }
             else
             {
-                return View(Models.DeviceProfile.IndexModel.Build(dbContext));
+                var m = Models.DeviceProfile.IndexModel.Build(dbContext);
+
+                // UI Extensions
+                UIExtensions.ExecuteExtensions<ConfigDeviceProfileIndexModel>(this.ControllerContext, m);
+
+                return View(m);
             }
         }
 
         public virtual ActionResult Create()
         {
-            return View();
+            var m = new Models.DeviceProfile.CreateModel();
+
+            // UI Extensions
+            UIExtensions.ExecuteExtensions<ConfigDeviceProfileCreateModel>(this.ControllerContext, m);
+
+            return View(m);
         }
 
         [HttpPost]
-        public virtual ActionResult Create(Disco.Models.Repository.DeviceProfile model)
+        public virtual ActionResult Create(Models.DeviceProfile.CreateModel model)
         {
             if (ModelState.IsValid)
             {
                 // Check for Existing
-                var existing = dbContext.DeviceProfiles.Where(m => m.Name == model.Name).FirstOrDefault();
+                var existing = dbContext.DeviceProfiles.Where(m => m.Name == model.DeviceProfile.Name).FirstOrDefault();
                 if (existing == null)
                 {
-                    model.ProvisionADAccount = true;
+                    model.DeviceProfile.ProvisionADAccount = true;
 
-                    dbContext.DeviceProfiles.Add(model);
+                    dbContext.DeviceProfiles.Add(model.DeviceProfile);
                     dbContext.SaveChanges();
-                    return RedirectToAction(MVC.Config.DeviceProfile.Index(model.Id));
+                    return RedirectToAction(MVC.Config.DeviceProfile.Index(model.DeviceProfile.Id));
                 }
                 else
                 {
                     ModelState.AddModelError("Name", "A Device Profile with this name already exists.");
                 }
             }
+
+            // UI Extensions
+            UIExtensions.ExecuteExtensions<ConfigDeviceProfileCreateModel>(this.ControllerContext, model);
 
             return View(model);
         }
@@ -92,6 +110,9 @@ namespace Disco.Web.Areas.Config.Controllers
             };
             m.DeviceProfilesAndNone = m.DeviceProfiles.ToList();
             m.DeviceProfilesAndNone.Insert(0, new Disco.Models.Repository.DeviceProfile() { Id = 0, Name = "<No Default>" });
+
+            // UI Extensions
+            UIExtensions.ExecuteExtensions<ConfigDeviceProfileDefaultsModel>(this.ControllerContext, m);
 
             return View(m);
         }

Disco.Web/Areas/Config/Views/DeviceProfile/Create.cshtml
@@ -1,4 +1,4 @@
-﻿@model Disco.Models.Repository.DeviceProfile
+﻿@model Disco.Web.Areas.Config.Models.DeviceProfile.CreateModel
 @{
     ViewBag.Title = Html.ToBreadcrumb("Configuration", MVC.Config.Config.Index(), "Device Profiles", MVC.Config.DeviceProfile.Index(null), "Create");
 }
@@ -11,21 +11,21 @@
                     Name:
                 </th>
                 <td>
-                @Html.TextBoxFor(model => model.Name)<br />@Html.ValidationMessageFor(model => model.Name)
+                @Html.TextBoxFor(model => model.DeviceProfile.Name)<br />@Html.ValidationMessageFor(model => model.DeviceProfile.Name)
                 </td>
             </tr>
             <tr>
                 <th>
                     Short Name:
                 </th>
-                <td>@Html.TextBoxFor(model => model.ShortName)<br />@Html.ValidationMessageFor(model => model.ShortName)
+                <td>@Html.TextBoxFor(model => model.DeviceProfile.ShortName)<br />@Html.ValidationMessageFor(model => model.DeviceProfile.ShortName)
                 </td>
             </tr>
             <tr>
                 <th>
                     Description:
                 </th>
-                <td>@Html.TextBoxFor(model => model.Description)<br />@Html.ValidationMessageFor(model => model.Description)
+                <td>@Html.TextBoxFor(model => model.DeviceProfile.Description)<br />@Html.ValidationMessageFor(model => model.DeviceProfile.Description)
                 </td>
             </tr>
         </table>

Disco.Web/Areas/Config/Views/DeviceProfile/Create.generated.cs
@@ -2,7 +2,7 @@
 //------------------------------------------------------------------------------
 // <auto-generated>
 //     This code was generated by a tool.
-//     Runtime Version:4.0.30319.17929
+//     Runtime Version:4.0.30319.18033
 //
 //     Changes to this file may cause incorrect behavior and will be lost if
 //     the code is regenerated.
@@ -31,9 +31,9 @@ namespace Disco.Web.Areas.Config.Views.DeviceProfile
     using Disco.Web;
     using Disco.Web.Extensions;
     
-    [System.CodeDom.Compiler.GeneratedCodeAttribute("RazorGenerator", "1.5.0.0")]
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("RazorGenerator", "1.5.4.0")]
     [System.Web.WebPages.PageVirtualPathAttribute("~/Areas/Config/Views/DeviceProfile/Create.cshtml")]
-    public class Create : System.Web.Mvc.WebViewPage<Disco.Models.Repository.DeviceProfile>
+    public partial class Create : System.Web.Mvc.WebViewPage<Disco.Web.Areas.Config.Models.DeviceProfile.CreateModel>
     {
         public Create()
         {
@@ -71,7 +71,7 @@ WriteLiteral("                ");
 
             
             #line 14 "..\..\Areas\Config\Views\DeviceProfile\Create.cshtml"
-           Write(Html.TextBoxFor(model => model.Name));
+           Write(Html.TextBoxFor(model => model.DeviceProfile.Name));
 
             
             #line default
@@ -80,7 +80,7 @@ WriteLiteral("<br />");
 
             
             #line 14 "..\..\Areas\Config\Views\DeviceProfile\Create.cshtml"
-                                                      Write(Html.ValidationMessageFor(model => model.Name));
+                                                                    Write(Html.ValidationMessageFor(model => model.DeviceProfile.Name));
 
             
             #line default
@@ -90,7 +90,7 @@ WriteLiteral("\r\n                </td>\r\n            </tr>\r\n            <tr>
 
             
             #line 21 "..\..\Areas\Config\Views\DeviceProfile\Create.cshtml"
-               Write(Html.TextBoxFor(model => model.ShortName));
+               Write(Html.TextBoxFor(model => model.DeviceProfile.ShortName));
 
             
             #line default
@@ -99,7 +99,7 @@ WriteLiteral("<br />");
 
             
             #line 21 "..\..\Areas\Config\Views\DeviceProfile\Create.cshtml"
-                                                               Write(Html.ValidationMessageFor(model => model.ShortName));
+                                                                             Write(Html.ValidationMessageFor(model => model.DeviceProfile.ShortName));
 
             
             #line default
@@ -110,7 +110,7 @@ WriteLiteral("\r\n                </td>\r\n            </tr>\r\n            <tr>
 
             
             #line 28 "..\..\Areas\Config\Views\DeviceProfile\Create.cshtml"
-               Write(Html.TextBoxFor(model => model.Description));
+               Write(Html.TextBoxFor(model => model.DeviceProfile.Description));
 
             
             #line default
@@ -119,7 +119,7 @@ WriteLiteral("<br />");
 
             
             #line 28 "..\..\Areas\Config\Views\DeviceProfile\Create.cshtml"
-                                                                 Write(Html.ValidationMessageFor(model => model.Description));
+                                                                               Write(Html.ValidationMessageFor(model => model.DeviceProfile.Description));
 
             
             #line default

Disco.Web/Areas/Config/Views/DeviceBatch/Create.cshtml
@@ -1,4 +1,4 @@
-﻿@model Disco.Models.Repository.DeviceBatch
+﻿@model Disco.Web.Areas.Config.Models.DeviceBatch.CreateModel
 @{
     ViewBag.Title = Html.ToBreadcrumb("Configuration", MVC.Config.Config.Index(), "Device Batches", MVC.Config.DeviceBatch.Index(null), "Create");
 }
@@ -11,14 +11,14 @@
                     Name:
                 </th>
                 <td>
-                    @Html.EditorFor(model => model.Name)<br />@Html.ValidationMessageFor(model => model.Name)
+                    @Html.EditorFor(model => model.DeviceBatch.Name)<br />@Html.ValidationMessageFor(model => model.DeviceBatch.Name)
                 </td>
             </tr>
             <tr>
                 <th>
                     Purchase Date:
                 </th>
-                <td>@Html.EditorFor(model => model.PurchaseDate)<br />@Html.ValidationMessageFor(model => model.PurchaseDate)
+                <td>@Html.EditorFor(model => model.DeviceBatch.PurchaseDate)<br />@Html.ValidationMessageFor(model => model.DeviceBatch.PurchaseDate)
                 </td>
             </tr>
         </table>

Disco.Web/Areas/Config/Views/DeviceBatch/Create.generated.cs
@@ -2,7 +2,7 @@
 //------------------------------------------------------------------------------
 // <auto-generated>
 //     This code was generated by a tool.
-//     Runtime Version:4.0.30319.17929
+//     Runtime Version:4.0.30319.18033
 //
 //     Changes to this file may cause incorrect behavior and will be lost if
 //     the code is regenerated.
@@ -31,9 +31,9 @@ namespace Disco.Web.Areas.Config.Views.DeviceBatch
     using Disco.Web;
     using Disco.Web.Extensions;
     
-    [System.CodeDom.Compiler.GeneratedCodeAttribute("RazorGenerator", "1.5.0.0")]
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("RazorGenerator", "1.5.4.0")]
     [System.Web.WebPages.PageVirtualPathAttribute("~/Areas/Config/Views/DeviceBatch/Create.cshtml")]
-    public class Create : System.Web.Mvc.WebViewPage<Disco.Models.Repository.DeviceBatch>
+    public partial class Create : System.Web.Mvc.WebViewPage<Disco.Web.Areas.Config.Models.DeviceBatch.CreateModel>
     {
         public Create()
         {
@@ -71,7 +71,7 @@ WriteLiteral("                    ");
 
             
             #line 14 "..\..\Areas\Config\Views\DeviceBatch\Create.cshtml"
-               Write(Html.EditorFor(model => model.Name));
+               Write(Html.EditorFor(model => model.DeviceBatch.Name));
 
             
             #line default
@@ -80,7 +80,7 @@ WriteLiteral("<br />");
 
             
             #line 14 "..\..\Areas\Config\Views\DeviceBatch\Create.cshtml"
-                                                         Write(Html.ValidationMessageFor(model => model.Name));
+                                                                     Write(Html.ValidationMessageFor(model => model.DeviceBatch.Name));
 
             
             #line default
@@ -91,7 +91,7 @@ WriteLiteral("\r\n                </td>\r\n            </tr>\r\n            <tr>
 
             
             #line 21 "..\..\Areas\Config\Views\DeviceBatch\Create.cshtml"
-               Write(Html.EditorFor(model => model.PurchaseDate));
+               Write(Html.EditorFor(model => model.DeviceBatch.PurchaseDate));
 
             
             #line default
@@ -100,7 +100,7 @@ WriteLiteral("<br />");
 
             
             #line 21 "..\..\Areas\Config\Views\DeviceBatch\Create.cshtml"
-                                                                 Write(Html.ValidationMessageFor(model => model.PurchaseDate));
+                                                                             Write(Html.ValidationMessageFor(model => model.DeviceBatch.PurchaseDate));
 
             
             #line default

commit ed87b27708cff56f765b4d217b676b271673d293

Fix: Device Profile Creation
Regression caused by b17ed91fb00c6606df94b407fc0355a8b43deb76

Disco.BI/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.0603.2017")]
-[assembly: AssemblyFileVersion("1.2.0603.2017")]
+[assembly: AssemblyVersion("1.2.0606.1156")]
+[assembly: AssemblyFileVersion("1.2.0606.1156")]

Disco.Data/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.0603.2017")]
-[assembly: AssemblyFileVersion("1.2.0603.2017")]
+[assembly: AssemblyVersion("1.2.0606.1156")]
+[assembly: AssemblyFileVersion("1.2.0606.1156")]

Disco.Models/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.0603.2017")]
-[assembly: AssemblyFileVersion("1.2.0603.2017")]
+[assembly: AssemblyVersion("1.2.0606.1156")]
+[assembly: AssemblyFileVersion("1.2.0606.1156")]

Disco.Services/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.0603.2017")]
-[assembly: AssemblyFileVersion("1.2.0603.2017")]
+[assembly: AssemblyVersion("1.2.0606.1156")]
+[assembly: AssemblyFileVersion("1.2.0606.1156")]

Disco.Web.Extensions/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.0603.2017")]
-[assembly: AssemblyFileVersion("1.2.0603.2017")]
+[assembly: AssemblyVersion("1.2.0606.1156")]
+[assembly: AssemblyFileVersion("1.2.0606.1156")]

Disco.Web/Areas/Config/Controllers/DeviceProfileController.cs
@@ -10,6 +10,7 @@ using Disco.Services.Plugins.Features.CertificateProvider;
 using Disco.Services.Plugins;
 using Disco.Services.Plugins.Features.UIExtension;
 using Disco.Models.UI.Config.DeviceProfile;
+using Disco.Models.Repository;
 
 namespace Disco.Web.Areas.Config.Controllers
 {
@@ -65,7 +66,14 @@ namespace Disco.Web.Areas.Config.Controllers
 
         public virtual ActionResult Create()
         {
-            var m = new Models.DeviceProfile.CreateModel();
+            var m = new Models.DeviceProfile.CreateModel()
+            {
+                DeviceProfile = new DeviceProfile()
+                {
+                    ComputerNameTemplate = "DeviceProfile.ShortName + '-' + SerialNumber",
+                    ProvisionADAccount = true
+                }
+            };
 
             // UI Extensions
             UIExtensions.ExecuteExtensions<ConfigDeviceProfileCreateModel>(this.ControllerContext, m);

Disco.Web/Areas/Config/Views/DeviceProfile/Create.cshtml
@@ -29,8 +29,8 @@
                 </td>
             </tr>
         </table>
-        <input type="hidden" name="ComputerNameTemplate" value="DeviceProfile.ShortName + '-' + SerialNumber" />
-        <input type="hidden" name="ProvisionADAccount" value="True" />
+        @Html.HiddenFor(model => model.DeviceProfile.ComputerNameTemplate)
+        @Html.HiddenFor(model => model.DeviceProfile.ProvisionADAccount)
         <p class="actions">
             <input type="submit" class="button" value="Create" />
         </p>

Disco.Web/Areas/Config/Views/DeviceProfile/Create.generated.cs
@@ -31,7 +31,7 @@ namespace Disco.Web.Areas.Config.Views.DeviceProfile
     using Disco.Web;
     using Disco.Web.Extensions;
     
-    [System.CodeDom.Compiler.GeneratedCodeAttribute("RazorGenerator", "1.5.4.0")]
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("RazorGenerator", "2.0.0.0")]
     [System.Web.WebPages.PageVirtualPathAttribute("~/Areas/Config/Views/DeviceProfile/Create.cshtml")]
     public partial class Create : System.Web.Mvc.WebViewPage<Disco.Web.Areas.Config.Models.DeviceProfile.CreateModel>
     {
@@ -124,23 +124,29 @@ WriteLiteral("<br />");
             
             #line default
             #line hidden
-WriteLiteral("\r\n                </td>\r\n            </tr>\r\n        </table>\r\n        <input");
+WriteLiteral("\r\n                </td>\r\n            </tr>\r\n        </table>\r\n");
 
-WriteLiteral(" type=\"hidden\"");
+WriteLiteral("        ");
 
-WriteLiteral(" name=\"ComputerNameTemplate\"");
+            
+            #line 32 "..\..\Areas\Config\Views\DeviceProfile\Create.cshtml"
+   Write(Html.HiddenFor(model => model.DeviceProfile.ComputerNameTemplate));
 
-WriteLiteral(" value=\"DeviceProfile.ShortName + \'-\' + SerialNumber\"");
+            
+            #line default
+            #line hidden
+WriteLiteral("\r\n");
 
-WriteLiteral(" />\r\n        <input");
+WriteLiteral("        ");
 
-WriteLiteral(" type=\"hidden\"");
+            
+            #line 33 "..\..\Areas\Config\Views\DeviceProfile\Create.cshtml"
+   Write(Html.HiddenFor(model => model.DeviceProfile.ProvisionADAccount));
 
-WriteLiteral(" name=\"ProvisionADAccount\"");
-
-WriteLiteral(" value=\"True\"");
-
-WriteLiteral(" />\r\n        <p");
+            
+            #line default
+            #line hidden
+WriteLiteral("\r\n        <p");
 
 WriteLiteral(" class=\"actions\"");
 

Disco.Web/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.2.0603.2017")]
-[assembly: AssemblyFileVersion("1.2.0603.2017")]
+[assembly: AssemblyVersion("1.2.0606.1156")]
+[assembly: AssemblyFileVersion("1.2.0606.1156")]