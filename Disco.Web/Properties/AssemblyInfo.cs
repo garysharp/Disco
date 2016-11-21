a0d643eda1617156e3c6d1fc5ffc1e1f6dc4964f
Gary Sharp <discoict@outlook.com>
2016-11-21 16:40:50 +1100

    Version bump for #103

--- Disco.Web/Properties/AssemblyInfo.cs
+++ Disco.Web/Properties/AssemblyInfo.cs
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2.2.16320.1300")]
-[assembly: AssemblyFileVersion("2.2.16320.1300")]
+[assembly: AssemblyVersion("2.2.16326.0500")]
+[assembly: AssemblyFileVersion("2.2.16326.0500")]
