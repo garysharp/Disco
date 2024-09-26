09f9f2d42711f3a8934d0a448e2b64d703f5a1d6
Gary Sharp <garypsharp@hotmail.com>
2024-09-26 16:33:55 +1000

    version bump for v2.4 release

--- Disco.Services.Plugins.ManifestGenerator/Properties/AssemblyInfo.cs
+++ Disco.Services.Plugins.ManifestGenerator/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.3.23161.0650")]
-[assembly: AssemblyFileVersion("2.3.23161.0650")]
+[assembly: AssemblyVersion("2.4.24270.0000")]
+[assembly: AssemblyFileVersion("2.4.24270.0000")]
